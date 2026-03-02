ef790af91a32b63638832ffd687967cab69844df
jailtonmonteiro <jailtonmonteiro147@gmail.com>
2026-03-02 12:55:22 -0300

    feat: Arquivo de exploração para entender algumas caracteristicas da base.

---
+++ notebooks/01_exploracao.py
@@ -0,0 +1,25 @@
+# %%
+import pandas as pd
+
+base =  pd.read_csv("../data/raw/base_chamados.csv", sep=";")
+base.head()
+# %%
+base.info()
+# %%
+base.groupby(by="empresa", as_index=False).count()
+
+# %%
+base.info()
+
+# %%
+base["data_fechamento"].min()
+# %%
+base["data_fechamento"].max()
+# %%
+base.groupby(by="setor").count()
+# %%
+base.groupby(by="categoria").count()
+
+# %%
+base.groupby(by="status").count()
+# %%
